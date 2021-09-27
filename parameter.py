35250945ac2997d4bd90a746b6284fe7805edd8a
Ironprop-Stone <zshi0616@mail.sdu.edu.cn>
2021-09-27 20:36:53 +0800

    update

--- parameter.py
+++ parameter.py
@@ -3,8 +3,8 @@ connect_gap_range = [10, 10]
 min_max_no_nodes_level = [10, 300]
 
 max_diff_range = [500, 500]
-max_tot_nodes = 5000
-min_tot_nodes = 2000
+max_tot_nodes = 3000
+min_tot_nodes = 1000
 
 # level_range = [3, 8]
 # connect_gap_range = [10, 10]
